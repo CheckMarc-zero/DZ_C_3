648f2ad302a2b14b5301b9ee75a796aec1d4692e
SandyAndy <ansigida77@gmail.com>
2022-10-12 22:31:24 +0300

    Задача 3

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,31 @@
+﻿int N = insertNumber("Введите число N:");
+
+int insertNumber(string massege)
+{
+Console.WriteLine(massege);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+if (N > 1)
+{
+Console.WriteLine("Табица кубов от 1 до N:");
+
+for (int i = 1; i<= N; i++)
+{
+    int cube = i*i*i;
+    Console.Write(cube);
+    if (i != N) Console.Write(",");
+}
+}
+else
+{
+   Console.WriteLine("Табица кубов от N до 1:");
+
+for (int i = N; i<= 1; i++)
+{
+    int cube = i*i*i;
+    Console.Write(cube);
+    if (i != 1) Console.Write(", ");
+} 
+}
