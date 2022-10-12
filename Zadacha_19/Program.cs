2ac5141b7b69f29459873ed14be69275fd01008b
SandyAndy <ansigida77@gmail.com>
2022-10-12 20:50:54 +0300

    Задача 1

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = insertNumber("Введите пятизначное число:");
+
+if (okNumber(a)) Console.WriteLine("Неверно введено число.");
+else 
+{
+    if (palindromOk(a)) Console.WriteLine("Введённое число - палиндром.");
+    else Console.WriteLine("Введённое число - не палиндром.");
+}
+
+int insertNumber(string massege)
+{
+Console.WriteLine(massege);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+bool okNumber(int n)
+{
+    if (n/100000 != 0 || n/10000 == 0) return true;
+    else return false;
+}
+
+bool palindromOk (int n)
+{
+    if (n/10000 == n%10 && (n/1000)%10 == (n%100)/10) return true;
+    else return false;
+}
