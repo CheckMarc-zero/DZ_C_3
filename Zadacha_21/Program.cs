068af5cea69ac86af72e3883dbf38fccbdd511f4
SandyAndy <ansigida77@gmail.com>
2022-10-12 22:02:06 +0300

    Задача 2

---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,34 @@
+﻿int [] A = vvodArray("Введите координаты точки А:");
+
+int [] B = vvodArray("Введите координаты точки В:");
+
+double sum = 0;
+
+for (int i = 0; i<=2; i++)
+{
+    sum = sum + (B[i]-A[i])*(B[i]-A[i]);
+}
+
+double dlinnaAB = Math.Sqrt(sum);
+
+Console.Write("Расстояние между А и В равно ");
+Console.WriteLine(sum);
+
+
+int insertNumber(string massege)
+{
+Console.WriteLine(massege);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int [] vvodArray (string massege)
+{
+    Console.WriteLine(massege);
+    int [] N = new int[3];
+    N[0] = insertNumber("Введите х:");
+     N[1] = insertNumber("Введите y:");
+      N[2] = insertNumber("Введите z:");
+      return N;
+}
+
